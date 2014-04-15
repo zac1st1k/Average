cafa54d99023ae3ba6d8c48029c75e31538c8cf8
Zac <zac@192-168-1-9.tpgi.com.au>
2014-04-15 11:54:26 +1000

    Initial Commit

---
+++ Average/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Average
+//
+//  Created by Zac on 15/04/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
