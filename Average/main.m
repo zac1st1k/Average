993e3f312ab720f9a8da5a07589fdd96999a3f8f
zac1st <zacfirst@gmail.com>
2014-04-16 11:30:12 +1000

    C average

--- Average/main.m
+++ Average/main.m
@@ -13,10 +13,31 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
+        double a, b;
+        int check;
+        int value;
+        int count;
         // insert code here...
-        NSLog(@"Hello, World!");
+        id someObject, someString, someNumber, someValue;
+        NSArray *someArray = [NSArray arrayWithObjects:someObject, someString, someNumber, someValue, nil];
+        //NSUInteger numberOfItems = [someArray count];
+        NSLog(@"count=%lu", (unsigned long)[someArray count]);
+        NSLog(@"\nHello, World!");
+        check = scanf("%d%d", &count,&value);
+        NSLog(@"%d", check);
+        printf("Printf Hello World!\n");
+
+        NSLog(@"\nEnter first number ");
+        scanf("%lf", &a);
         
+        NSLog(@"Enter second number ");
+        scanf("%lf", &b);
+        
+        NSLog(@"Average = %f", (a + b) / 2);    
+        
+
     }
     return 0;
 }
+
 
